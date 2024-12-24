9b34f20153c1326f637b683ee8f89f4afea2d344
Esraa <Abdo@DESKTOP-6H9B8GJ>
2024-12-24 15:02:49 +0200

    Editing Models

--- SocialConnect/Program.cs
+++ SocialConnect/Program.cs
@@ -1,4 +1,12 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SocialConnect.Models;
+using SocialConnect.UnitOfWorks;
+using System.Text;
+
 namespace SocialConnect
 {
     public class Program
@@ -12,7 +20,31 @@ namespace SocialConnect
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(op => op.EnableAnnotations());
+            builder.Services.AddDbContext<SocialConnectDBContext>(option => option.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("con")));
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<SocialConnectDBContext>();
+            builder.Services.AddAuthentication(option => {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(
+            //validate token
+            op =>
+            {
+                op.SaveToken = true;
+                #region secret key
+                string key = "A_Secure_256_Bit_Key_That_Is_32_Bytes_Long";
+                var secertkey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key));
+                #endregion
+                op.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = secertkey,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            builder.Services.AddScoped<UnitOfWork>();
 
             var app = builder.Build();
 
